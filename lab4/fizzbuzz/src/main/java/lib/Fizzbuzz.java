af67a178af69ee5336f65701079e8665eee8744a
KingRosay_13 <monzasango2@gmail.com>
2026-06-26 14:04:43 -0400

    Vert: test9 reussi
    [INFO]  T E S T S
    [INFO] -------------------------------------------------------
    [INFO] Running test.FizzbuzzTest
    [INFO] Tests run: 5, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.015 s - in test.FizzbuzzTest
    [INFO]
    [INFO] Results:
    [INFO]
    [INFO] Tests run: 5, Failures: 0, Errors: 0, Skipped: 0
    [INFO]
    [INFO] ------------------------------------------------------------------------
    [INFO] BUILD SUCCESS
    [INFO] ------------------------------------------------------------------------
    [INFO] Total time:  0.895 s
    [INFO] Finished at: 2026-06-26T14:04:43-04:00
    [INFO] ------------------------------------------------------------------------

--- lab4/fizzbuzz/src/main/java/lib/Fizzbuzz.java
+++ lab4/fizzbuzz/src/main/java/lib/Fizzbuzz.java
@@ -10,6 +10,7 @@ public class Fizzbuzz {
         if (n == 2) return "2";
         if (n == 3) return "Fizz";
         if (n == 6) return "Fizz";
+        if (n == 9) return "Fizz";
         return null;
     }
     
